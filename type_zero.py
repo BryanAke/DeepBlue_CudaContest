dc3c851bc34d58578c383555faa27d949a8353d6
George Macon <george.macon@gmail.com>
2011-11-11 22:18:50 -0500

    Removing things for speed.

--- type_zero.py
+++ type_zero.py
@@ -1,14 +1,16 @@
 from SimpleXMLRPCServer import SimpleXMLRPCServer, SimpleXMLRPCDispatcher
 
-import logging
-from logging import debug, info, warning, error, exception, critical
+#import logging
+#from logging import debug, info, warning, error, exception, critical
 
+import collections
 import random
+import traceback
 
 LISTEN = ('0.0.0.0', 1337)
 
-root = logging.getLogger("")
-root.setLevel(logging.INFO)
+#root = logging.getLogger("")
+#root.setLevel(logging.INFO)
 
 #ch = logging.StreamHandler()
 #ch.setLevel(logging.INFO)
@@ -18,13 +20,13 @@ root.setLevel(logging.INFO)
 
 class RackO(object):
 	def _push_discard(self, card):
-		debug("Added %d to the discard pile", card)
+		#debug("Added %d to the discard pile", card)
 		self.discard.append(card)
-		self.deck.discard(card)
+		#self.deck.discard(card)
 
 	def _pop_discard(self):
 		card = self.discard.pop()
-		debug("Took %d from the discard pile", card)
+		#debug("Took %d from the discard pile", card)
 		return card
 
 	def _print_game_state(self):
@@ -39,74 +41,77 @@ class RackO(object):
 		print "Discard pile contains", self.discard
 
 	def ping(self, p):
-		debug("ping(%s)", repr(p))
+		#debug("ping(%s)", repr(p))
 		try:
 			return "pong"
 		except Exception:
-			exception("")
+			traceback.print_exc()
+			#exception("")
 			raise
 
 	def start_game(self, args):
-		debug("start_game(%s)", repr(args))
+		#debug("start_game(%s)", repr(args))
 		try:
-			self.game_id = args['game_id']
-			self.player_id = args['player_id']
-			self.discard = []
-			self.other_player_id = args['other_player_id']
+			#self.game_id = args['game_id']
+			#self.player_id = args['player_id']
+			self.discard = collections.deque()
+			#self.other_player_id = args['other_player_id']
 
-			self.deck = set(range(1, 81))
-			self.deck_size = 80 - 20 - 20 - 1
+			#self.deck = set(range(1, 81))
+			#self.deck_size = 80 - 20 - 20 - 1
 			self.rack = []
 
 			self.moves = 0
 
-			self.other_rack = [None] * 20
+			#self.other_rack = [None] * 20
 
-			info("Starting game %d going %s against team %d.", self.game_id, ("first", "second")[self.player_id], self.other_player_id)
+			#info("Starting game %d going %s against team %d.", self.game_id, ("first", "second")[self.player_id], self.other_player_id)
 
 			self._push_discard(args['initial_discard'])
 
 			return ""
 		except Exception:
-			exception("")
+			traceback.print_exc()
+			#exception("")
 			raise
 
 	def get_move(self, args):
-		debug("get_move(%s)", repr(args))
+		#debug("get_move(%s)", repr(args))
 		try:
 			self.moves += 1
 
-			if args['game_id'] != self.game_id:
-				error("Got a request for a move in non-active game.")
+			#if args['game_id'] != self.game_id:
+				#error("Got a request for a move in non-active game.")
 
 			if self.rack:
 				if self.rack != args['rack']:
-					error("Our rack is out-of-sync!")
+					print("Our rack is out-of-sync!")
 					self.rack = args['rack']
 			else:
 				self.rack = args['rack']
-				self.deck.difference_update(self.rack)
+				#self.deck.difference_update(self.rack)
 
-			self.timeleft = args['remaining_microseconds']
+			#self.timeleft = args['remaining_microseconds']
+			print "{:7d}".format(args['remaining_microseconds'])
 
 			if args['other_player_moves']:
 				other_move = args['other_player_moves'][0][1]
 				if other_move['move'] == 'take_discard':
 					card = self._pop_discard()
-					self.other_rack[other_move['idx']] = card
-					debug("The other player took %d and put it in slot %d.", card, other_move['idx'])
-				elif other_move['move'] == 'take_deck':
-					self.other_rack[other_move['idx']] = 0
-					self.deck_size -= 1
-					debug("The other player drew and put it in slot %d.", other_move['idx'])
-				elif other_move['move'] == 'no_move':
-					debug("The other player made no move.")
-				elif other_move['move'] == 'illegal':
-					debug("The other player made an illegal move: %s.", other_move['reason'])
-				elif other_move['move'] == 'timed_out':
-					debug("The other player timed out.")
-				else:
-					error("The other player did something unknown!")
+					#self.other_rack[other_move['idx']] = card
+					#debug("The other player took %d and put it in slot %d.", card, other_move['idx'])
+				#elif other_move['move'] == 'take_deck':
+					#self.other_rack[other_move['idx']] = 0
+					#self.deck_size -= 1
+					#debug("The other player drew and put it in slot %d.", other_move['idx'])
+				#elif other_move['move'] == 'no_move':
+					#debug("The other player made no move.")
+				#elif other_move['move'] == 'illegal':
+					#debug("The other player made an illegal move: %s.", other_move['reason'])
+				#elif other_move['move'] == 'timed_out':
+					#debug("The other player timed out.")
+				#else:
+					#error("The other player did something unknown!")
 
 				self._push_discard(args['discard'])
 
@@ -117,68 +122,70 @@ class RackO(object):
 
 			return our_move
 		except Exception:
-			exception("")
+			traceback.print_exc()
+			#exception("")
 			raise
 
 	def get_deck_exchange(self, args):
-		debug("get_deck_exchange(%s)", repr(args))
+		#debug("get_deck_exchange(%s)", repr(args))
 		try:
-			if args['game_id'] != self.game_id:
-				error("Got a request for a move in non-active game.")
+			#if args['game_id'] != self.game_id:
+				#error("Got a request for a move in non-active game.")
 
-			self.timeleft = args['remaining_microseconds']
+			#self.timeleft = args['remaining_microseconds']
 
 			self.card = args['card']
 
-			self.deck_size -= 1
-			self.deck.discard(self.card)
-
-			self.new_rack = self.rack[:]
+			#self.deck_size -= 1
+			#self.deck.discard(self.card)
 
 			# Level -1 Play: Insert in random location
 			self.idx = random.randint(0, 19)
 
 			return self.idx
 		except Exception:
-			exception("")
+			traceback.print_exc()
+			#exception("")
 			raise
 
 	def move_result(self, args):
-		debug("move_result(%s)", repr(args))
+		#debug("move_result(%s)", repr(args))
 		try:
-			if args['game_id'] != self.game_id:
-				error("Got a request for a move in non-active game.")
+			#if args['game_id'] != self.game_id:
+			#	error("Got a request for a move in non-active game.")
 
 			move = args['move']
 			if move == 'next_player_turn':
-				debug("We moved successfully")
+				#debug("We moved successfully")
 				self._push_discard(self.rack[self.idx])
 				self.rack[self.idx] = self.card
-			elif move == 'move_ended_game':
-				debug("The game is over: %s", args['reason'])
+			#elif move == 'move_ended_game':
+				#debug("The game is over: %s", args['reason'])
 			elif move == 'illegal':
-				debug("We made an illegal move: %s", args['reason'])
+				print("We made an illegal move: %s", args['reason'])
 			elif move == 'timed_out':
-				error("We timed out.")
-			else:
-				error("We did something unexpected.")
+				print("We timed out.")
+			#else:
+				#error("We did something unexpected.")
 
 			return ""
 		except Exception:
-			exception("")
+			traceback.print_exc()
+			#exception("")
 			raise
 
 	def game_result(self, args):
-		debug("game_result(%s)", repr(args))
+		#debug("game_result(%s)", repr(args))
 		try:
-			if args['game_id'] != self.game_id:
-				error("Got a request for a move in non-active game.")
+			#if args['game_id'] != self.game_id:
+				#error("Got a request for a move in non-active game.")
 
-			info("The game is over after %d moves: %d - %d because %s", self.moves, args['your_score'], args['other_score'], args['reason'])
+			print("The game is over after %d moves: %d - %d because %s", self.moves, args['your_score'], args['other_score'], args['reason'])
 
 			return ""
 		except Exception:
-			exception("")
+			traceback.print_exc()
+			#exception("")
 			raise
 
 server = SimpleXMLRPCServer(LISTEN, logRequests=False)
