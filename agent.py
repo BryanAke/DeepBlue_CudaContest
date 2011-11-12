c83943d58b8094e6f7ef862d3621eb658724bfd1
James Donald <jndonald3@gmail.com>
2011-11-12 03:03:27 -0500

    adjusting should draw

--- agent.py
+++ agent.py
@@ -1,4 +1,5 @@
 import random
+import knowledge_base
 
 class agent(object):
     
@@ -6,7 +7,20 @@ class agent(object):
         self.knowledge = knowledgeBase
         
     def should_draw(self):
-        return true
+        ## get the top of the discard pile
+        topCard = -1
+        if(len(self.knowledge.discard_pile) > 0):
+            topCard = self.knowledge.discard_pile[0]
+        if(topCard >= 0):
+            ## greedily find ordered index (least)
+            orderedIndex = random.randint(0, 20)
+            ## check happiness at that index
+            happy = 1.0 - abs(((topCard/kCardCount) - ((orderedIndex+1)/kRackSize)))
+            ## check if happy is greater than threshhold
+            if(happy > kOk):
+                return true
+            else:
+                return false
     
     def place_card(self, card):
         return random.randint(0, 19)
