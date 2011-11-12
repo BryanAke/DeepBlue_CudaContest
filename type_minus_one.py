4a60ef1942e760571ffcbb15eacc6cf265d9564b
Bryan Ake <Bryan.Ake@gatech.edu>
2011-11-11 22:56:21 -0500

    setsockopt

--- type_minus_one.py
+++ type_minus_one.py
@@ -1,6 +1,25 @@
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 
 import random
+import socket
+
+# This is a hack to patch slow socket.getfqdn calls that
+# BaseHTTPServer (and its subclasses) make.
+# See: http://bugs.python.org/issue6085
+# See: http://www.answermysearches.com/xmlrpc-server-slow-in-python-how-to-fix/2140/
+
+import BaseHTTPServer
+
+def _bare_address_string(self):
+    host, port = self.client_address[:2]
+    return '%s' % host
+
+BaseHTTPServer.BaseHTTPRequestHandler.address_string = \
+        _bare_address_string
+
+# End hack.
+
+
 
 LISTEN = ('0.0.0.0', 1337)
 
@@ -12,7 +31,7 @@ class RackO(object):
 		return ""
 
 	def get_move(self, args):
-		print "{:7d}".format(args['remaining_microseconds'])
+		#print "{:7d}".format(args['remaining_microseconds'])
 		return { 'move' : 'request_deck' }
 
 	def get_deck_exchange(self, args):
@@ -33,5 +52,6 @@ class RackO(object):
 		return ""
 
 server = SimpleXMLRPCServer(LISTEN, logRequests=False)
+server.socket.setsockopt(socket.SOL_TCP, socket.TCP_NODELAY, 1)
 server.register_instance(RackO())
 server.serve_forever()
